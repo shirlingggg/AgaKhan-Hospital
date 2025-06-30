78b945e0d5108b0ebc9614e2fd8193d0ad131143
shirlingggg <shirleenkabunyi@gmail.com>
2025-06-30 11:47:14 +0300

    initial

---
+++ myapp/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def starter(request):
+    return render(request, 'starter-page.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def service(request):
+    return render(request, 'services.html')
+
+def doctors(request):
+    return render(request, 'doctors.html')
+
+def appointment(request):
+    return render(request, 'appointment.html')
